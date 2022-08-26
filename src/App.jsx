9af1dc63dcf660b2ce7a74608812b947aafe48a4
Carlos Eduardo <cadu4duarte@gmail.com>
2022-08-26 18:45:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Main} from "./pages/Main";
+import {Repositorio} from "./pages/Repositorio";
+ 
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main/>}/>
+        <Route exact path="/repositorio/:repositorio" element={<Repositorio/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
