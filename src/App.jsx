5292afa2674dac16e6d8482fd1136d70610d0caa
Carlos Eduardo <cadu4duarte@gmail.com>
2022-08-26 19:40:48 -0300

    feat: import global styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Main} from "./pages/Main";
 import {Repositorio} from "./pages/Repositorio";
+import GlobalStyle from "./styles/global"
  
 
 function App() {
 
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Routes>
         <Route exact path="/" element={<Main/>}/>
         <Route exact path="/repositorio/:repositorio" element={<Repositorio/>}/>
