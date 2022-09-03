cd4221b5835e61b72efb1f18a963353ae386d2c7
Carlos Eduardo <cadu4duarte@gmail.com>
2022-09-03 18:30:08 -0300

    feat: save repository

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa"
 import {Container, Form, SubmitButton, List, DeleteButton} from "./styles";
 import api from "../../service/api";
@@ -7,14 +7,41 @@ export function Main() {
   const [newRepo, setNewRepo] = useState("");
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  //buscando repositórios salvos
+  useEffect(() => {
+    const repoStorage = localStorage.getItem("repos");
+
+    if (repoStorage) {
+      setRepositorios(JSON.parse(repoStorage));
+    }
+  }, []);
+  
+  //salvando repositorios
+  useEffect(() => {
+    localStorage.setItem("repos", JSON.stringify(repositorios));
+  }, [repositorios]);
 
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
 
     async function submit() {
       setLoading(true);
+      setAlert(null);
       try {
+        if(newRepo === "") {
+          throw new Error("precisa digitar algo para fazer a busca");
+        }
+
         const response = await api.get(`repos/${newRepo}`);
+
+        //evitando que seja salvo repositórios iguais
+        const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+        if(hasRepo) {
+          throw new Error("Repositório duplicado");
+        }
 
         const data = {
           name: response.data.full_name,
@@ -24,6 +51,7 @@ export function Main() {
         setRepositorios([...repositorios, data]);
         setNewRepo(""); 
         } catch(error) {
+          setAlert(true);
           console.log(error);
         } finally {
           setLoading(false);
@@ -35,6 +63,7 @@ export function Main() {
 
   function handleinputChange(e) {
     setNewRepo(e.target.value);
+    setAlert(null);
   }
 
   //usando use callback por estar manipulando um state
@@ -51,7 +80,7 @@ export function Main() {
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={handleSubmit }>
+      <Form onSubmit={handleSubmit } error={alert}>
         <input 
         type="text" 
         placeholder="Adicionar repositório"
